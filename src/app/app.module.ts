64acc3fc016c7dd2422409c21ffd8788cbe10b8e
Riyasheri <Riyariyaaddon2022@gmail.com>
2022-07-07 22:15:23 +0530

    doctors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { SearchdepartmentComponent } from './searchdepartment/searchdepartment.component';
+import { ViewdoctorsComponent } from './viewdoctors/viewdoctors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontPageComponent,
-    SearchdepartmentComponent
+    SearchdepartmentComponent,
+    ViewdoctorsComponent
   ],
   imports: [
     BrowserModule,
